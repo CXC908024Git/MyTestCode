ab05bcce2db0023e6e42b7b67fdff3d31b29b459
cxc <cxc908024@163.com>
2019-07-16 19:26:21 +0800

    add by xccai @20190716 for closefile

---
+++ std.h
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<math.h>
+#include<string.h>
+#include<errno.h>
+#include<string.h>
+#include<stdarg.h>
+#include<sys/stat.h>
+#include<time.h>
+#include<sys/timeb.h>
