c8ba20b710945b1a0eaf8f8c2f433441a9439fde
cxc <cxcubuntu@163.com>
2019-10-12 16:22:50 +0800

    mod by cxc

--- sedpubforlinux.sh
+++ sedpubforlinux.sh
@@ -5,8 +5,14 @@
 
 ask_mode()
 {
-    echo "请选择sed的操作模式（0-字符串全篇替换;1-特定行删除;2-特定行行尾追加;3-特定行前追加;4-特定行后追加;5-只匹配特定行的第一次后追加
-         6-只匹配特定行的最后一次后追加)"
+    echo "请选择sed的操作模式:
+0-字符串全篇替换
+1-特定行删除
+2-特定行行尾追加
+3-特定行前追加
+4-特定行后追加
+5-只匹配特定行的第一次后追加
+6-只匹配特定行的最后一次后追加)"
     read mode_sel
     if [ $mode_sel -eq 0 ];then
         sed  "s|cxc|xccai|g" testsedpub.txt >> testtemp
@@ -18,26 +24,18 @@ ask_mode()
         sed "/kil/s|$|& TAIL|g" testsedpub.txt >> testtemp
         mv testtemp testsedpub.txt
     elif [ $mode_sel -eq 3 ];then
-        sed "/kil/i\\
-        abc
-        " testsedpub.txt >> testtemp
+        sed "/kil/i abc\nmnk" testsedpub.txt >> testtemp 
         mv testtemp testsedpub.txt
     elif [ $mode_sel -eq 4 ];then
-        sed "/kil/a\\
-        xyz
-        " testsedpub.txt >> testtemp
+        sed "/kil/a xyz\nstu" testsedpub.txt >> testtemp
         mv testtemp testsedpub.txt
     elif [ $mode_sel -eq 5 ];then
         linenum=`grep -n kil testsedpub.txt.base|awk -F ":" '{if(NR==1)print $1}'`
-        sed "${linenum}a\\
-        xyz
-        " testsedpub.txt >> testtemp
+        sed "${linenum}i abconce" testsedpub.txt >> testtemp
         mv testtemp testsedpub.txt
     elif [ $mode_sel -eq 6 ];then
         linenum=`grep -n kil testsedpub.txt.base|sed -n '$p'|awk -F ":" '{print $1}'`
-        sed "${linenum}a\\
-        xyz
-        " testsedpub.txt >> testtemp
+        sed "${linenum}a xyzonce" testsedpub.txt >> testtemp
         mv testtemp testsedpub.txt
     else
     	echo "暂不支持其它其它模式"
