8b109a78992a6f914ca547a17045e98b76545490
xccai <cxc908024@163.com>
2020-06-03 01:18:00 +0800

    add by xccai for eclipase

---
+++ test0603/src/test0603/AutoCar.java
@@ -0,0 +1,23 @@
+package test0603;
+
+class Car
+{
+	static String part1 = "油门踏板";
+	static String part2 = "刹车踏板";
+	static void  oper(Car c)
+	{
+		System.out.println("右脚踩" + part1 + "!");
+		System.out.println("右脚踩" + part2 + "!");
+	}
+			
+}
+
+public class AutoCar extends Car {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AutoCar ac = new AutoCar();
+		System.out.println("自动挡汽车有2块踏板。");
+		oper(ac);
+	}
+}
