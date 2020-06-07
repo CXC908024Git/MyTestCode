17d694477449cc3fbc1209953de972fd7471738d
xccai <cxc908024@163.com>
2020-06-07 23:13:48 +0800

    llllll

---
+++ test0603001/src/test0603001/Compare.java
@@ -0,0 +1,20 @@
+package test0603001;
+
+public class Compare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = Integer.parseInt("345");
+		
+		Integer t1 = new Integer(345);
+		
+		System.out.println(t1.equals(a));
+		
+		System.out.println(Integer.toBinaryString(a));
+		System.out.println(Integer.toHexString(a));
+		System.out.println(Integer.toOctalString(a));
+		System.out.println(Integer.toString(a, 15));	
+
+	}
+
+}
